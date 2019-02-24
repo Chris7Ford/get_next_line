98892fcdc105e852c60a4176d67f8df11de9e8af
Christopher Ford <chford@e1z2r4p27.42.us.org>
2019-02-24 13:40:45 -0800

    Redo get next line so it does not use a linked list
    sucessfully passing a simple two-line test so far. More to come
    Need to correct the return vals

--- get_next_line.c
+++ get_next_line.c
@@ -6,135 +6,128 @@
 /*   By: chford <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/21 09:59:02 by chford            #+#    #+#             */
-/*   Updated: 2019/02/22 17:26:32 by chford           ###   ########.fr       */
+/*   Updated: 2019/02/24 13:39:37 by chford           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
 #include "get_next_line.h"
 
-int		fill_list(const int fd, t_list **list)
+int		count_line_chars(char *string, int i)
 {
-	char	buffer[BUF_SIZE + 1];
-	int		new_line_found;
-	int		ret;
-	int		i;
-
-	new_line_found = 0;
-	while ((ret = read(fd, buffer, BUF_SIZE)) && !new_line_found)
-	{
-		buffer[ret] = '\0';
-		ft_lstpush(list, (void *)buffer, ret);
-		i = 0;
-		while (buffer[i])
-		{
-			if (buffer[i] == '\n')
-			{
-				new_line_found = 1;
-				break ;
-			}
-			i++;
-		}
-	}
-	if (ret == -1)
-		return (0);
-	return (1);
-}
-
-t_position	get_index_and_link(t_list **list, int pos)
-{
-	t_position	position_data;
-	t_list		*elem;
-	int			char_count;
-
-	char_count = 0;
-	position_data.link = 0;
-	while (char_count + BUF_SIZE < pos && elem)
-	{
-		char_count += BUF_SIZE;
-		elem = elem->next;
-		position_data.link++;
-	}
-	position_data.i = pos - char_count;
-	return (position_data);
-}
-
-int		count_line_chars(t_list **list, int pos)
-{
-	t_position		position_data;
-	t_list			*elem;
 	int				count;
 
-	elem = *list;
 	count = 0;
-	position_data = get_index_and_link(list, pos);
-	while (elem)
+	while (string[i])
 	{
-		while (((char *)(elem->content))[position_data.i])
-		{
-			if (((char *)(elem->content))[position_data.i] == '\n')
-				return (count);
-			position_data.i++;
-			count++;
-		}
-		position_data.i = 0;
-		elem = elem->next;
+		if (string[i] == '\n')
+			return (count * -1);
+		i++;
+		count++;
 	}
 	return (count);
 }
 
-int		fill_string(char ** line, t_list **list, int pos)
+char	*ft_strjoin_nl(char *s1, char *s2)
 {
-	t_position		position_data;
-	t_list			*elem;
-	int				link;
-	int				j;
+	int		i;
+	int		j;
+	int		l;
+	char	*string;
 
-	elem = *list;
-	link = -1;
+	if (!s1 || !s2)
+		return (0);
+	i = count_line_chars(s1, 0);
+	j = count_line_chars(s2, 0);
+	i = (i < 0) ? (i * -1) : i;
+	j = (j < 0) ? (j * -1) : j;
+	string = (char *)malloc(sizeof(char) * (i + j));
+	if (!string)
+		return (0);
+	i = 0;
 	j = 0;
-	position_data = get_index_and_link(list, pos);
-	while (++link < position_data.link && elem)
-		elem = elem->next;
-	while (elem)
+	l = 0;
+	while (s1[l] != '\0')
+		string[i++] = s1[l++];
+	l = 0;
+	while (s2[l] != '\0')
+		string[i + j++] = s2[l++];
+	string[i + j] = '\0';
+	return (string);
+}
+
+char	*prepare_s1(t_cursor fd_store, int length)
+{
+	int				i;
+	int				j;
+	char			*s1;
+
+	i = fd_store.value;
+	j = 0;
+	length = (length < 0) ? length * -1 : length;
+	s1 = (char *)malloc(sizeof(char) * (length + 1));
+	while (j < length)
 	{
-		while (((char *)(elem->content))[position_data.i])
-		{
-			if (((char *)(elem->content))[position_data.i] == '\n')
-			{
-				(*line)[j] = '\0';
-				return (1);
-			}
-			(*line)[j++] = ((char *)(elem->content))[position_data.i++];
-		}
-		position_data.i = 0;
-		elem = elem->next;
+		s1[j] = fd_store.last[j + i];
+		j++;
 	}
-	(*line)[j] = '\0';
-	return (1);
+	s1[j] = '\0';
+	return (s1);
+}
+
+char	*prepare_s2(t_cursor *fd_store, int fd)
+{
+	char	buffer[BUFF_SIZE + 1];
+	char	*tmp;
+	char	*s2;
+	int		length;
+	int		ret;
+
+	s2 = (char *)malloc(sizeof(char) * 1);
+	s2[0] = '\0';
+	while ((ret = read(fd, buffer, BUFF_SIZE)) && buffer[ret - 1] != '\n')
+	{
+		buffer[ret] = '\0';
+		tmp = ft_strdup(s2);
+		ft_strcpy(fd_store->last, buffer);
+		length = count_line_chars(buffer, 0);
+		length = (length < 0) ? (length * -1) : length;
+		free (s2);
+		s2 = ft_strjoin_nl(tmp, buffer);
+		free(tmp);
+		tmp = 0;
+	}
+	fd_store->value += length;
+	return (s2);
 }
 
 int		get_next_line(const int fd, char **line)
 {
 	static t_cursor	fd_store[FD_MAX + 1];
-	t_list			*list;
+	char			*s1;
+	char			*s2;
 	int				length;
-	int				pos;
 
-	list = 0;
+	s1 = 0;
 	if ((fd_store[fd]).touched == 1)
-		pos = (fd_store[fd]).value;
-	else
 	{
-		pos = 0;
-		(fd_store[fd]).value = 0;
-		(fd_store[fd]).touched = 1;
+		length = count_line_chars((fd_store[fd]).last, (fd_store[fd]).value);
+		s1 = prepare_s1(fd_store[fd], length);
+		if (length < 0)
+		{
+			fd_store[fd].value += (length * -1);
+			*line = s1;
+			return (1);
+		}
 	}
-	if (!(fill_list(fd, &list)))
-		return (-1);
-	length = count_line_chars(&list, pos);
+	else
+		(fd_store[fd]).value = 0;
+	(fd_store[fd]).touched = 1;
+	s2 = prepare_s2(&(fd_store[fd]), fd);
+	length = count_line_chars(s2, 0);
+	*line = (s1 && s2) ? ft_strjoin(s1, s2) : ft_strdup(s2);
+	free(s1);
+	free(s2);
 	(fd_store[fd]).value += length + 1;
-	*line = (char *)malloc(sizeof(char) * (length + 1));
-	fill_string(line, &list, pos);
 	return (0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,34 +6,24 @@
 /*   By: chford <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/21 09:42:05 by chford            #+#    #+#             */
-/*   Updated: 2019/02/22 17:27:34 by chford           ###   ########.fr       */
+/*   Updated: 2019/02/24 12:23:08 by chford           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define	FD_MAX 1024
-# define	BUF_SIZE 1
+# define	FD_MAX 1023
+# define	BUFF_SIZE 1
 typedef struct s_cursor		t_cursor;
-typedef struct s_position	t_position;
 
 struct						s_cursor
 {
 	int						value;
 	char					touched;
-	char					*last;
+	char					last[BUFF_SIZE];
 };
 
-struct						s_position
-{
-	int						i;
-	int						link;
-};
-
-t_position	get_index_and_link(t_list **list, int pos);
-int			fill_list(const int fd, t_list **list);
-int			count_line_chars(t_list **list, int pos);
-int			fill_string(char ** line, t_list **list, int pos);
+int			count_line_chars(char *string, int i);
 int			get_next_line(const int fd, char **line);
 #endif
